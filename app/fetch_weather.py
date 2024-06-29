05df95c615aa058afb4c48382f05ce7e2310bc16
Yousaf Bin Farrukh <yousafbinfarrukh@gmail.com>
2024-06-29 13:49:58 +0500

    Added cache based on number of requests.

--- app/fetch_weather.py
+++ app/fetch_weather.py
@@ -2,10 +2,16 @@ from fastapi import HTTPException
 import httpx
 import os
 from dotenv import load_dotenv
+from functools import lru_cache
+
+@lru_cache(maxsize=100)
+def get_weather_data(endpoint: str, params: dict) -> dict:
+    response = httpx.get(endpoint, params=params)
+    print('Made a call')
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Failed to fetch weather data")
+    return response.json()
 
 async def fetch_weather_data(endpoint: str, params: dict) -> dict:
-    async with httpx.AsyncClient() as client:
-        response = await client.get(endpoint, params=params)
-        if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Failed to fetch weather data")
-        return response.json()
+    return get_weather_data(endpoint, tuple(sorted(params.items())))
+
